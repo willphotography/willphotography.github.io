31629a4987a4c6f456d470eeab5feec107d13a6e
willphotography <willwellsy@gmail.com>
2026-09-13 18:38:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ console.log("Portfolio loaded");
 window.addEventListener("contextmenu", function(e) {
   e.preventDefault();
 });
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+
+document.querySelectorAll(".gallery img").forEach(img => {
+  img.addEventListener("click", () => {
+    lightbox.style.display = "flex";
+    lightboxImg.src = img.src;
+  });
+});
+
+lightbox.addEventListener("click", () => {
+  lightbox.style.display = "none";
+});
